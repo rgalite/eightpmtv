e66d0c54cfd5888e268975e0408b3806e1540426
RudthMael <crystalgold@Hermes.local>
2010-10-05 20:51:23 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tvshows::Application.config.secret_token = 'ab8a0b5f3f5fd60dcf04dc1a58f2d33c2720783967598fbf429c6f7eb3e45bff33afb309687510adb5ea07b3a4f42f92e4b272fd345614fa9c102fb4e9318d4e'
