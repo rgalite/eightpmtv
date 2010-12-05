70374f7700541c8d68abe3cd44c1d017a552fb35
RudthMael <crystalgold@Hermes.local>
2010-12-05 18:47:34 +0100

    Added user_id unique for likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,6 @@
 class Like < ActiveRecord::Base
   belongs_to :likeable, :polymorphic => true
-  validates_inclusion_of :value, :in => %w(0 1), :allow_nil => false
-  validates_uniqueness_of :user_id, :scope => [ :likeable_id, :likeable_type ]
+
+  validates :value, :inclusion => { :in => %w(0 1) }
+  validates :user_id, :uniqueness => { :scope => [ :likeable_id, :likeable_type ] }
 end
