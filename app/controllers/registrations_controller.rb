8802491b9404446bea5c97d0648f6649dfaa3bf0
RudthMael <crystalgold@Hermes.local>
2011-01-21 23:21:51 +0100

    Removed rake tasks and added new asset host

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   def create
     if session[:omniauth] == nil #OmniAuth
-      if verify_recaptcha
-        super
-        session[:omniauth] = nil unless @user.new_record? #OmniAuth
-      else
-        build_resource
-        clean_up_passwords(resource)
-        flash[:error] = "There was an error with the recaptcha code below. Please re-enter the code and click submit."
-        render_with_scope :new
-      end
+      build_resource
+      clean_up_passwords(resource)
+      flash[:error] = "There was an error with the recaptcha code below. Please re-enter the code and click submit."
+      render_with_scope :new
     else
       super
       session[:omniauth] = nil unless @user.new_record? #OmniAuth
