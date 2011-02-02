6ca024d5d1521fb7a9a49aca7d6e1b347d6c726c
RudthMael <crystalgold@Hermes.local>
2011-02-02 21:08:24 +0100

    Fixed view and action when user follows another

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,15 +28,28 @@ class UsersController < ApplicationController
       fw = current_user.follow(@user)
       UserMailer.delay.deliver_new_follower(@user.id, current_user.id) if fw && !fw.blocked
       redirect_to user_path(@user), :notice => "Congratulations! You are now following #{@user.username}!"
+      a_data = {}
+      a_data[:user_path] = user_path(@user)
+      a_data[:user_name] = @user.full_name
+      a_data[:user_img] = @user.photo.url(:thumb)
+                 
+      # Get the series the user is following
+      a_data[:user_series] = @user.series.all.randomly_pick(5).collect{ |serie| { :name => serie.full_name, :path => show_path(serie) } }
+      a_data[:user_series_count] = @user.series.count
+
+      # Get the people who are following the user
+      a_data[:user_followers] = @user.followers.randomly_pick(5).collect{ |follower| { :name => follower.full_name, :path => user_path(follower), :img => follower.avatar_url(:thumb) } }
+      a_data[:user_followers_count] = @user.followers.count
+
+      # Get the people the user is following
+      a_data[:user_followings] = @user.following_by_type('User').randomly_pick(5).collect{ |follower| { :name => follower.full_name, :path => user_path(follower), :img => follower.avatar_url(:thumb) } }
+      a_data[:user_followings_count] = @user.following_by_type('User').count
+
       a = current_user.activities.create!(:actor_path => user_path(current_user),
                                           :actor_img => current_user.photo.url(:thumb),
                                           :subject => fw,
                                           :kind => "follow_user",
-                                          :data => { :user_path => user_path(@user),
-                                                     :user_name => @user.full_name,
-                                                     :user_img => @user.photo.url(:thumb) }.to_json)
-    else
-      redirect_to user_path(@user), :warn => "Uh oh. It's like you're already following #{@user.username}."
+                                          :data => a_data.to_json)
     end
   end
   
