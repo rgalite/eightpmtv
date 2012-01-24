a7e0438ce942c37f46bd0a79e5ca2fa74f2e7e08
Rudth-Mael Galite <rudth.mael.galite@gmail.com>
2012-01-24 12:27:49 +0100

    Fixed production assets path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Tvshows::Application.configure do
   config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "http://assets.eightpm.tv.s3.amazonaws.com/"
+  config.action_controller.asset_host = "http://assets.eightpm.tv.s3.amazonaws.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
